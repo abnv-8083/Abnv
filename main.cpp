d8d519755b6ba956ea49b150c4bb52b8d757021f
abnv-8083 <106509244+abnv-8083@users.noreply.github.com>
2024-01-24 21:10:23 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+        int day;
+        cout << "Enter the day (1-31) : ";
+        cin >> day;
+        if (day == 1)
+                cout << "Monday";
+        else if (day == 2)
+                cout << "Tuesday";
+        else if (day == 3)
+                cout << "Wednesday";
+        else if (day == 4)
+                cout << "Thursday";
+         else if (day == 5)
+                cout << "Friday";
+         else if (day == 6)
+                cout << "Saturday";
+         else if (day == 7)
+                cout << "Sunday";
+         else if (day == 8)
+                cout << "Monday";
+        else if (day == 9)
+                cout << "Tuesday";
+        else if (day == 10)
+                cout << "Wednesday";
+        else if (day == 11)
+                cout << "Thursday";
+        else if (day == 12)
+                cout << "Friday";
+        else if (day == 13)
+                cout << "Saturday";
+        else if (day == 14)
+                cout << "Sunday";
+        else if (day == 15)
+                cout << "Monday";
+        else if (day == 16)
+                cout << "Tuesday";
+        else if (day == 17)
+                cout << "Wednesday";
+        else if (day == 18)
+                cout << "Thursday";
+        else if (day == 19)
+                cout << "Friday";
+        else if (day == 20)
+                cout << "Saturday";
+        else if (day == 21)
+                cout << "Sunday";
+        else if (day == 22)
+                cout << "Monday";
+        else if (day == 23)
+                cout << "Tuesday";
+        else if (day == 24)
+                cout << "Wednesday";
+        else if (day == 25)
+                cout << "Thursday";
+        else if (day == 26)
+                cout << "Friday";
+        else if (day == 27)
+                cout << "Saturday";
+        else if (day == 28)
+                cout << "Sunday";
+        else if (day == 29)
+                cout << "Monday";
+        else if (day == 30)
+                cout << "Tuesday";
+        else if (day == 31)
+                cout << "Wednesday";
+        else
+                cout << "Wrong input";
+        return 0;
+}
